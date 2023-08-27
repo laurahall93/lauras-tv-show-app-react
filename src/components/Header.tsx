58a53cb845e8a5bdb6e1f978e81981023d5ea6b8
Laura Hall <podinjshmi1993@gmail.com>
2023-08-27 18:57:02 +0100

    base code fir the movie search app

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header(): JSX.Element {
+  return (
+    <div>
+      <h1 className="title">Movie search</h1>
+    </div>
+  );
+}
---
+++ src/components/MainBody.tsx
@@ -0,0 +1,41 @@
+import { EpisodesEntry } from "./EpisodesEntry";
+import episodes from "../episodes.json";
+import { useState } from "react";
+
+export function MainBody(): JSX.Element {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(event.target.value);
+  };
+  const searchEpisodeData = episodes.filter(
+    (episode) =>
+      episode.name.toLowerCase().includes(searchInput) ||
+      episode.summary.toLowerCase().includes(searchInput)
+  );
+
+  const listEpisodes = searchEpisodeData.map((episode) => (
+    <div key={episode.id}>
+      <EpisodesEntry
+        name={episode.name}
+        season={episode.season}
+        number={episode.number}
+        image={episode.image}
+        url={episode.url}
+        summary={episode.summary}
+      />
+    </div>
+  ));
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="search a movie or tv show"
+        value={searchInput}
+        onChange={handleChange}
+      />
+      <div className="episode-container">{listEpisodes}</div>
+    </>
+  );
+}
